3942691a0d6fcb862cd0ef98b5099293b0c80c7f
RoadToHelen <119435758+RoadToHelen@users.noreply.github.com>
2023-02-21 22:29:15 +0300

    Update bot.py
    add class VkBot

--- bot.py
+++ bot.py
@@ -27,43 +27,42 @@ class VkBot:
     def send_some_msg(self, user_id, some_text):
         self.vk.method('messages.send', {'user_id': user_id, 'message': some_text, 'random_id': randrange(10**7)})
 
+    def get_user_name(self, user_id):
+        param = {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'}
+        method = 'users.get'
+        rec = requests.get(url=f'https://api.vk.com/method/{method}', params=param)
+        response = rec.json()
+        dict_user_info = response['response']
+        try:
+            for i in dict_user_info:
+                for key, value in i.items():
+                    first_name = i.get('first_name')
+                    return first_name
+        except KeyError:
+            self.send_some_msg(user_id, 'Ошибка')
+
+    def get_user_info(self, user_id):
+        param = {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'}
+        method = 'users.get'
+        rec = requests.get(url=f'https://api.vk.com/method/{method}', params=param)
+        response = rec.json()
+        dict_user_info = response['response']
+        try:
+            for i in dict_user_info:
+                for key, value in i.items():
+                    first_name = i.get('first_name')
+                    last_name = i.get('last_name')
+                    bdate = i.get('bdate')
+                    city = i.get('city')
+                    sex = i.get('sex')
+                    relation = i.get('relation')
+                    user_dict = {'first_name': first_name, 'last_name': last_name, 'bdate': bdate, 'city': city, 'sex': sex, 'relation': relation}
+                    return user_dict
+        except KeyError:
+            self.send_some_msg(user_id, 'Ошибка')
+
 
 VkBot = VkBot()
-
-
-# class VkUser:
-#     url = 'https://api.vk.com/method/'
-#
-#     def __init__(self, user_token, user_id, version):
-#         self.params = {'access_token': user_token,
-#                        'user_ids': user_id,
-#                        'v': version
-#                        }
-
-
-def get_user_info(user_id):
-    param = {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'}
-    method = 'users.get'
-    rec = requests.get(url=f'https://api.vk.com/method/{method}', params=param)
-    response = rec.json()
-    dict_user_info = response['response']
-    pprint(dict_user_info)
-
-
-def get_user_name(user_id):
-    param = {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'}
-    method = 'users.get'
-    rec = requests.get(url=f'https://api.vk.com/method/{method}', params=param)
-    response = rec.json()
-    dict_user_info = response['response']
-    pprint(dict_user_info)
-    try:
-        for i in dict_user_info:
-            for key, value in i.items():
-                first_name = i.get('first_name')
-                return first_name
-    except KeyError:
-        VkBot.send_some_msg(user_id, 'Ошибка')
 
 
 for event in VkBot.longpoll.listen():
@@ -72,12 +71,12 @@ for event in VkBot.longpoll.listen():
         request = event.text.lower()
         user_id = str(event.user_id)
         if msg == 'hi':
-            VkBot.send_some_msg(user_id, f'Hi, {get_user_name(user_id)}, if you want to pick up a pair - type  "start search"')
+            VkBot.send_some_msg(user_id, f'Hi, {VkBot.get_user_name(user_id)}, if you want to pick up a pair - type  "start search"')
         elif request == 'привет':
-            VkBot.send_some_msg(user_id, f'Привет, {get_user_name(user_id)}! Если хочешь подобрать пару - набери "начать поиск"')
+            VkBot.send_some_msg(user_id, f'Привет, {VkBot.get_user_name(user_id)}! Если хочешь подобрать пару - набери "начать поиск"')
         elif request == 'start search':
-            VkBot.send_some_msg(user_id, f'Start searching')
+            VkBot.send_some_msg(user_id, f'Start searching for {VkBot.get_user_info(user_id)}')
         elif request == 'начать поиск':
-            VkBot.send_some_msg(user_id, f'{get_user_name(user_id)}, начинаю поиск')
+            VkBot.send_some_msg(user_id, f'{VkBot.get_user_name(user_id)}, начинаю поиск по параметрам {VkBot.get_user_info(user_id)}')
         else:
-            VkBot.send_some_msg(user_id, f'{get_user_name(user_id)}, твое сообщение не понятно')
+            VkBot.send_some_msg(user_id, f'{VkBot.get_user_name(user_id)}, твое сообщение не понятно')
