fc3bdafa1cff79bb413deb09cc11400cd84cffc5
RoadToHelen <119435758+RoadToHelen@users.noreply.github.com>
2023-03-25 16:34:58 +0300

    Update database.py
    drop  для проверки

--- database.py
+++ database.py
@@ -1,9 +1,4 @@
 import psycopg2
-# import sqlalchemy
-
-# DSN = 'postgresql://postgres:12345@localhost:5432/neto'
-# engine = sqlalchemy.create_engine(DSN)
-# connection = engine.connect()
 
 
 def create_db():
@@ -17,7 +12,6 @@ def create_db():
     initial_connection.close()
     print('БД neto создана')
 
-
 connection = psycopg2.connect(
     host='localhost',
     port='5432',
@@ -29,30 +23,49 @@ connection = psycopg2.connect(
 connection.autocommit = True
 
 def create_users():
-    with connection.cursor() as cursor:
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users(
-        vk_id VARCHAR(9) PRIMARY KEY,
-        first_name VARCHAR(20) NOT NULL,
-        last_name VARCHAR(20),
-        vk_link VARCHAR(30) NOT NULL
-        );
-    ''')
-    print('Таблица users создана')
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+            vk_id VARCHAR(9) PRIMARY KEY,
+            first_name VARCHAR(20) NOT NULL,
+            last_name VARCHAR(20)
+            );
+        ''')
+        print('Таблица users создана')
+    except KeyError:
+        return
 
-def insert_users(vk_id, first_name, last_name, vk_link):
-    with connection.cursor() as cursor:
-        cursor.execute('''
-        INSERT INTO users(vk_id, first_name, last_name, vk_link)
-    VALUES
-        ('{vk_id}', '{first_name}', '{last_name}', '{vk_link}'
-        );
-    ''')
+def insert_users(vk_id, first_name, last_name):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            INSERT INTO users(vk_id, first_name, last_name)
+        VALUES
+            ('{vk_id}', '{first_name}', '{last_name}'
+            );
+        ''')
+    except KeyError:
+        return
 
 def drop_users():
-    with connection.cursor() as cursor:
-        cursor.execute('''
-        DROP TABLE IF EXISTS users;
-        '''
-        )
-        print('Таблица users удалена')
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            DROP TABLE IF EXISTS users;
+            '''
+            )
+            print('Таблица users удалена')
+    except KeyError:
+        return
+
+def drop_db_users():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            DROP DATABASE IF EXISTS users;
+            '''
+            )
+            print('База данных users удалена')
+    except KeyError:
+        return
