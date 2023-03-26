3837cd06066e7b4ba3b4ad29a9c5f32225569476
RoadToHelen <119435758+RoadToHelen@users.noreply.github.com>
2023-03-27 02:20:04 +0300

    Update database.py

--- database.py
+++ database.py
@@ -11,85 +11,77 @@ def create_db():
     initial_connection.close()
     print('БД neto создана')
 
+connection = psycopg2.connect(
+    host='localhost',
+    port='5432',
+    user='postgres',
+    database='neto',
+    password='12345'
+)
 
-class DB:
-    def __init__(self, host, port, user, database, password):
-        self.connection = psycopg2.connect(
-            host='localhost',
-            port='5432',
-            user='postgres',
-            database='neto',
-            password='12345',
-        cursorclass = psycopg2.cursors.DictCursor
-        )
+connection.autocommit = True
 
-        self.connection.autocommit = True
 
-    def create_users(self):
-        try:
-            with self.connection.cursor() as cursor:
-                cursor.execute('''
-                CREATE TABLE IF NOT EXISTS users(
-                vk_id VARCHAR(9) PRIMARY KEY,
-                first_name VARCHAR(20) NOT NULL,
-                last_name VARCHAR(20)
-                );
-            ''')
-            print('Таблица users создана')
-        except KeyError:
-            return
-
-    def insert_users(self, vk_id, first_name, last_name):
-        try:
-            self.cursor = connection.cursor()
+def create_users():
+    try:
+        with connection.cursor() as cursor:
             cursor.execute('''
-                INSERT INTO users(vk_id, first_name, last_name)
+            CREATE TABLE IF NOT EXISTS users(
+            vk_id VARCHAR(9) PRIMARY KEY,
+            first_name VARCHAR(20) NOT NULL,
+            last_name VARCHAR(20)
+            );
+        ''')
+        print('Таблица users создана')
+    except KeyError:
+        return
+
+
+def insert_users(vk_id, first_name, last_name):
+    try:
+        cursor = connection.cursor()
+        cursor.execute('''
+            INSERT INTO users(vk_id, first_name, last_name)
             VALUES
-                (%s, %s, %s)
-                '''
-                % (vk_id, first_name, last_name),
-                )
-            self.connection.commit()
-            self.cursor.close()
-            print('user добавлен в таблицу')
-        except KeyError:
-            return
+            (%s, %s, %s);
+            '''
+            % (vk_id, first_name, last_name),
+        )
+        connection.commit()
+        cursor.close()
+        print('user добавлен в таблицу')
+    except KeyError:
+        return
 
 
-    def select_users(self):
-        try:
-            self.cursor = connection.cursor()
-            cursor.execute('''
-                SELECT vk_id FROM users
-                ''')
+def select_users():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT * FROM users''')
             print(cursor.fetchall())
-            self.connection.commit()
-            self.cursor.close()
-
-        except KeyError:
-            return
+    except KeyError:
+        return
 
 
-    def drop_users(self):
-        try:
-            with self.connection.cursor() as cursor:
-                cursor.execute('''
-                DROP TABLE IF EXISTS users;
-                '''
-                )
-                print('Таблица users удалена')
-        except KeyError:
-            return
+def drop_users():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            DROP TABLE IF EXISTS users;
+            '''
+            )
+            print('Таблица users удалена')
+    except KeyError:
+        return
 
-    def drop_db_users(self):
-        try:
-            with self.connection.cursor() as cursor:
-                cursor.execute('''
-                DROP DATABASE IF EXISTS users;
-                '''
-                )
-                print('База данных users удалена')
-        except KeyError:
-            return
 
-DB = DB()
+def drop_db_users():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            DROP DATABASE IF EXISTS users;
+            '''
+            )
+            print('База данных users удалена')
+    except KeyError:
+        return
