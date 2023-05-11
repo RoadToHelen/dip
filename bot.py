eac208bedc19e47de09db1727ad22e9aa3143062
RoadToHelen <119435758+RoadToHelen@users.noreply.github.com>
2023-05-11 21:51:47 +0300

    Update bot.py
    Api 3

--- bot.py
+++ bot.py
@@ -31,78 +31,78 @@ class VkBot:
             dict_user_name = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in dict_user_name:
-                for key, value in i.items():
-                    first_name = i.get('first_name')
-                    return first_name
+        for i in dict_user_name:
+            for key, value in i.items():
+                first_name = i.get('first_name')
+                return first_name
 
     def get_user_info(self, user_id):
         try:
             dict_user_info = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in dict_user_info:
-                for key, value in i.items():
-                    first_name = i.get('first_name')
-                    last_name = i.get('last_name')
-                    bdate = i.get('bdate')
-                    sex = i.get('sex')
-                    relation = i.get('relation')
-                    city = i.get('city')
-                    if 'city' in key:
-                        city = key.get('city')
-                        title = str(city.get('title'))
-                        return title
-                    # user_dict = {'first_name': first_name, 'last_name': last_name, 'bdate': bdate, 'city': str(city.get('title')), 'sex': sex, 'relation': relation}
-                    user_list = [user_id, first_name, last_name, bdate, str(city.get('title')), sex, relation]
-                    return user_list
+        for i in dict_user_info:
+            for key, value in i.items():
+                first_name = i.get('first_name')
+                last_name = i.get('last_name')
+                bdate = i.get('bdate')
+                sex = i.get('sex')
+                relation = i.get('relation')
+                city = i.get('city')
+                if 'city' in key:
+                    city = key.get('city')
+                    title = str(city.get('title'))
+                    return title
+                # user_dict = {'first_name': first_name, 'last_name': last_name, 'bdate': bdate, 'city': str(city.get('title')), 'sex': sex, 'relation': relation}
+                user_list = [user_id, first_name, last_name, bdate, str(city.get('title')), sex, relation]
+                return user_list
 
     def get_daiting_user_info(self, user_id):
         try:
             dict_duser_info = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate, city, sex, relation', 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in dict_duser_info:
-                for key, value in i.items():
-                    bdate = i.get('bdate')
-                    bdate_list = bdate.split('.')
-                    if len(bdate_list) == 2 or None:
-                        age = self.get_bdate(user_id)
-                    elif len(bdate_list) == 3:
-                        bday = int(bdate_list[0])
-                        bmonth = int(bdate_list[1])
-                        byear = int(bdate_list[2])
-                        day_today = int(datetime.date.today().day)
-                        month_today = int(datetime.date.today().month)
-                        year_today = int(datetime.date.today().year)
-                        if month_today > bmonth:
-                            age = year_today - byear
-                        elif (month_today == bmonth and bday > day_today) or (month_today == bmonth and bday > day_today):
-                            age = year_today - byear
-                        else:
-                            age = year_today - byear - 1
+        for i in dict_duser_info:
+            for key, value in i.items():
+                bdate = i.get('bdate')
+                bdate_list = bdate.split('.')
+                if len(bdate_list) == 2 or None:
+                    age = self.get_bdate(user_id)
+                elif len(bdate_list) == 3:
+                    bday = int(bdate_list[0])
+                    bmonth = int(bdate_list[1])
+                    byear = int(bdate_list[2])
+                    day_today = int(datetime.date.today().day)
+                    month_today = int(datetime.date.today().month)
+                    year_today = int(datetime.date.today().year)
+                    if month_today > bmonth:
+                        age = year_today - byear
+                    elif (month_today == bmonth and bday > day_today) or (month_today == bmonth and bday > day_today):
+                        age = year_today - byear
                     else:
-                        self.send_some_msg(user_id, 'Ошибка')
-                    sex = i.get('sex')
-                    if sex == 1:
-                        sex_name = 'мужчину'
-                        sex_find = 2
-                    elif sex == 2:
-                        sex_name = 'женщину'
-                        sex_find = 1
-                    else:
-                        VkBot.send_some_msg(user_id, 'Ваш пол не задан, кого ищем (мужчину/женщину): ')
-                        for event in self.longpoll.listen():
-                            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                                sex_name = event.text
-                    city = i.get('city')
-                    if 'city' in key:
-                        city = key.get('city')
-                        title = str(city.get('title'))
-                        return title
-                    dating_user_dict = {'age from': (age - 5), 'age to': (age + 5), 'city': str(city.get('title')),
-                                        'sex': sex_name, 'sex_find': sex_find}
-                    return self.send_some_msg(user_id, f"{self.get_user_name(user_id)}, ищем {dating_user_dict['sex']} от {dating_user_dict['age from']} до {dating_user_dict['age to']} лет из города {dating_user_dict['city']}?")
+                        age = year_today - byear - 1
+                else:
+                    self.send_some_msg(user_id, 'Ошибка')
+                sex = i.get('sex')
+                if sex == 1:
+                    sex_name = 'мужчину'
+                    sex_find = 2
+                elif sex == 2:
+                    sex_name = 'женщину'
+                    sex_find = 1
+                else:
+                    VkBot.send_some_msg(user_id, 'Ваш пол не задан, кого ищем (мужчину/женщину): ')
+                    for event in self.longpoll.listen():
+                        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                            sex_name = event.text
+                city = i.get('city')
+                if 'city' in key:
+                    city = key.get('city')
+                    title = str(city.get('title'))
+                    return title
+                dating_user_dict = {'age from': (age - 5), 'age to': (age + 5), 'city': str(city.get('title')),
+                                    'sex': sex_name, 'sex_find': sex_find}
+                return self.send_some_msg(user_id, f"{self.get_user_name(user_id)}, ищем {dating_user_dict['sex']} от {dating_user_dict['age from']} до {dating_user_dict['age to']} лет из города {dating_user_dict['city']}?")
 
     def get_bdate(self, user_id):
         self.send_some_msg(user_id, 'Введите свою дату рождения в формате дд.мм.гггг: ')
@@ -131,53 +131,53 @@ class VkBot:
             dating_sex_dict = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'fields': 'sex', 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in dating_sex_dict:
-                for key, value in i.items():
-                    sex = i.get('sex')
-                    if sex == 1:
-                        find = 2
-                        return find
-                    elif sex == 2:
-                        find = 1
-                        return find
+        for i in dating_sex_dict:
+            for key, value in i.items():
+                sex = i.get('sex')
+                if sex == 1:
+                    find = 2
+                    return find
+                elif sex == 2:
+                    find = 1
+                    return find
 
     def get_age(self, user_id):
         try:
             age_dict = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'fields': 'bdate', 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in age_dict:
-                for key, value in i.items():
-                    bdate = i.get('bdate')
-                    bdate_list = bdate.split('.')
-                    if len(bdate_list) == 2 or None:
-                        age = self.get_bdate(user_id)
-                    elif len(bdate_list) == 3:
-                        bday = int(bdate_list[0])
-                        bmonth = int(bdate_list[1])
-                        byear = int(bdate_list[2])
-                        day_today = int(datetime.date.today().day)
-                        month_today = int(datetime.date.today().month)
-                        year_today = int(datetime.date.today().year)
-                        if month_today > bmonth:
-                            age = year_today - byear
-                            return age
-                        elif (month_today == bmonth and bday > day_today) or (month_today == bmonth and bday > day_today):
-                            age = year_today - byear
-                            return age
-                        else:
-                            age = year_today - byear - 1
-                            return age
+        for i in age_dict:
+            for key, value in i.items():
+                bdate = i.get('bdate')
+                bdate_list = bdate.split('.')
+                if len(bdate_list) == 2 or None:
+                    age = self.get_bdate(user_id)
+                elif len(bdate_list) == 3:
+                    bday = int(bdate_list[0])
+                    bmonth = int(bdate_list[1])
+                    byear = int(bdate_list[2])
+                    day_today = int(datetime.date.today().day)
+                    month_today = int(datetime.date.today().month)
+                    year_today = int(datetime.date.today().year)
+                    if month_today > bmonth:
+                        age = year_today - byear
+                        return age
+                    elif (month_today == bmonth and bday > day_today) or (month_today == bmonth and bday > day_today):
+                        age = year_today - byear
+                        return age
+                    else:
+                        age = year_today - byear - 1
+                        return age
 
     def get_city(self, user_id):
         try:
             city_dict = self.vk2.method('users.get', {'access_token': user_token, 'user_ids': user_id, 'fields': 'city', 'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            for i in city_dict:
-                for key, value in i.items():
-                    city = i.get('city')
-                    return city
+        for i in city_dict:
+            for key, value in i.items():
+                city = i.get('city')
+                return city
 
 
     def get_dating_users(self, user_id, offset = 164):
@@ -191,21 +191,21 @@ class VkBot:
                                             'v': '5.131'})
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            du_list = daiting_user['items']
-            for i in du_list:
-                for key, value in i.items():
-                    global vk_id, first_name, last_name
-                    vk_id = i.get('id')
-                    first_name = i.get('first_name')
-                    last_name = i.get('last_name')
-                    is_closed = i.get('is_closed')
-                    bdate = i.get('bdate')
-                    city = i.get('city')
-                    if city == self.get_city(user_id) and is_closed == False:
-                        dating_dict = {'vk_id': vk_id, 'first_name': first_name, 'last_name': last_name,
-                                       'city': str(city.get('title')), 'bdate': bdate}
-                        dating_list = (vk_id, first_name, last_name)
-                        return dating_list
+        du_list = daiting_user['items']
+        for i in du_list:
+            for key, value in i.items():
+                global vk_id, first_name, last_name
+                vk_id = i.get('id')
+                first_name = i.get('first_name')
+                last_name = i.get('last_name')
+                is_closed = i.get('is_closed')
+                bdate = i.get('bdate')
+                city = i.get('city')
+                if city == self.get_city(user_id) and is_closed == False:
+                    dating_dict = {'vk_id': vk_id, 'first_name': first_name, 'last_name': last_name,
+                                   'city': str(city.get('title')), 'bdate': bdate}
+                    dating_list = (vk_id, first_name, last_name)
+                    return dating_list
 
     def get_dating_user(self, user_id):
         global duser_id
@@ -213,19 +213,19 @@ class VkBot:
             dating_list = self.get_dating_users(user_id)
         except ApiError:
             self.send_some_msg(user_id, 'Ошибка')
-            duser_id = dating_list[0]
-            # drop_users()
-            create_db()
-            create_users()
-            select_users()
-            checkes_users = check_users(duser_id)
-            check = {int(vk_id[0]) for duser_id in checkes_users}
-            if not check:
-                insert_users(dating_dict['vk_id'], dating_dict['first_name'], dating_dict['last_name'])
-                photos_list = self.get_photos(duser_id)
-                return self.send_some_msg(user_id, f'{dating_list[1]} {dating_list[2]}', photos_list)
-            else:
-                self.next(user_id)
+        duser_id = dating_list[0]
+        # drop_users()
+        create_db()
+        create_users()
+        select_users()
+        checkes_users = check_users(duser_id)
+        check = {int(vk_id[0]) for duser_id in checkes_users}
+        if not check:
+            insert_users(dating_dict['vk_id'], dating_dict['first_name'], dating_dict['last_name'])
+            photos_list = self.get_photos(duser_id)
+            return self.send_some_msg(user_id, f'{dating_list[1]} {dating_list[2]}', photos_list)
+        else:
+            self.next(user_id)
 
     if __name__ == '__main__':
         print('вход bot.py')
@@ -236,7 +236,7 @@ class VkBot:
             photos_list = photos['items']
             # pprint(photos_list)
         except ApiError:
-            return
+            self.send_some_msg(user_id, 'Ошибка')
 
         sorted_photos = sorted(photos_list, key=lambda x: x['likes']['count'], reverse=True)[:3]
         new_list = []
